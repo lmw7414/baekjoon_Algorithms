d10950dc5c3da008d97f6cd6e2b481f19398bbd8
lmw7414 <lmw7414@naver.com>
2022-07-06 15:40:42 +0900

    solve problem11729

--- src/baekjoon/problem11729.java
+++ src/baekjoon/problem11729.java
@@ -6,8 +6,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 //하노이 탑 이동순서
+//totalcount는 2^n - 1 공식을 사용해도 된다.
 public class problem11729 {
-
+    static int count =0;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -15,14 +16,13 @@ public class problem11729 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
 
-        System.out.println(totalCount(N));
         hanoi(N,1,2,3);
+        System.out.println(count);
         System.out.print(sb.toString());
     }
 
     static void hanoi(int n, int start, int via, int to) {
-
-
+        count++;
         if(n == 1){
             sb.append(start + " " + to + '\n');
             return;
