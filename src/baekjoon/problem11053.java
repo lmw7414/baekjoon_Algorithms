0041479a813975ed83110446a962769f55f4becf
Lee Min Woo <lmw7414@naver.com>
2022-09-02 20:53:28 +0900

    solve problem11053

---
+++ src/baekjoon/problem11053.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.util.Scanner;
+//가장 긴 증가하는 부분 수열
+// https://www.acmicpc.net/board/view/63273  -> 예제 1000개
+public class problem11053 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int arr[];
+        int result[];
+        arr = new int[N];
+        result = new int[N];
+        for(int i = 0; i< N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < N; i++) {
+            result[i] = 1;
+
+            for(int j = 0; j < i; j++) {
+                if(arr[j] < arr[i] && result[i] < result[j] + 1)
+                    result[i] = result[j] + 1;
+            }
+        }
+        int max = -1;
+        for(int i = 0; i< N; i++) {
+            if(max < result[i])
+                max = result[i];
+        }
+        System.out.println(max);
+    }
+}
