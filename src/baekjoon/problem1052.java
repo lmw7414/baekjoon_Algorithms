4bc6cf239b2fcea7294f8b7665cfdb6be25b8938
Lee Min Woo <lmw7414@naver.com>
2022-09-08 19:26:39 +0900

    solve problem1052

---
+++ src/baekjoon/problem1052.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class problem1052 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        System.out.println(calculation(N, K));
+    }
+
+    static int calculation(int n, int k) {
+        int bottle;
+        int count;
+        int buy = 0;
+
+        if(n == 0)
+            return 0;
+
+        while(true) {
+            bottle = n + buy;
+            count = 0;
+            while(bottle > 0) {
+                if(bottle %2 != 0)
+                    count++;
+                bottle/=2;
+            }
+            if(count <= k)
+                return buy;
+            buy++;
+        }
+
+    }
+}
