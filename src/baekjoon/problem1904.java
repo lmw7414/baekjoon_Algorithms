1fc905c364431d3cb7f53d23d07924b16b980b04
Lee Min Woo <lmw7414@naver.com>
2023-01-13 11:50:36 +0900

    solve problem1904

---
+++ src/baekjoon/problem1904.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class problem1904 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[n + 2];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
