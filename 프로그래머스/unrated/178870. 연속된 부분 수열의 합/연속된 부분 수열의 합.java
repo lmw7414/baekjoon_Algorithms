8e18e3a7a02b641d8b768859c1c92e6a6e262c45
이민우 <lmw7414@naver.com>
2023-04-12 14:11:04 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = null;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        while(true) {
+
+            if(end < sequence.length && sum <= k) {  // end 이동
+                sum += sequence[end++];
+                if(sum == k)
+                    answer = findOptimal(answer, start, end - 1);
+            }
+            if(start < sequence.length && sum > k) { // sum이 k 이상일 경우
+                sum -= sequence[start++];
+                if(sum == k)
+                    answer = findOptimal(answer, start, end - 1);
+            }
+            if(start == sequence.length - 1 && end == sequence.length)
+                break;
+            if(end == sequence.length && sum <= k)
+                break;
+
+        }
+        return answer;
+    }
+
+    public static int[] findOptimal(int[] answer, int start, int end) {
+        if(answer == null) {
+            answer = new int[] {start, end};
+            return answer;
+        }
+        if(answer[1] - answer[0] >= end - start) { // 길이 비교
+            if(answer[1] - answer[0] == end - start) { // 길이가 같을 때
+                if(answer[0] < start) {
+                    return answer;
+                } else {
+                    return new int[] {start, end};
+                }
+            } else {
+                return new int[] {start, end};
+            }
+        }
+        return answer;
+    }
+}
