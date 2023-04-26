7533b1d28714d3ed3d1e666a277f5a59722daace
이민우 <lmw7414@naver.com>
2023-04-26 16:27:39 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 15.87 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -0,0 +1,53 @@
+import java.util.*;
+class Solution {
+    public int[] solution(long[] numbers) {
+        List<Integer> answer = new ArrayList<>();
+
+        for(long number : numbers) {
+            String pBNum = toPBT(number);
+            int depth = findDepth(pBNum.length()) - 1;
+            boolean result = dfs(pBNum, pBNum.length()/2,  depth);
+            answer.add(result ? 1 : 0);
+        }
+
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static boolean dfs(String bNum, int rootIdx, int depth) {
+        int lIdx = rootIdx - (int)Math.pow(2, depth - 1);
+        int rIdx = rootIdx + (int)Math.pow(2, depth - 1);
+        if(depth == 0)
+            return true;
+        if(bNum.charAt(rootIdx) == '0') {
+            if(bNum.charAt(lIdx) == '1' || bNum.charAt(rIdx) == '1')
+                return false;
+        }
+
+        boolean left;
+        boolean right;
+        // 왼쪽
+        left = dfs(bNum, lIdx, depth - 1);
+        // 오른쪽
+        right = dfs(bNum, rIdx, depth - 1);
+
+        return left && right;
+    }
+
+    public static String toPBT(long num) {
+        String bNum = Long.toBinaryString(num);
+        int depth = findDepth(bNum.length());
+        long n = (long)Math.pow(2, depth) - 1;
+        String zero = "0".repeat((int)(n - bNum.length()));
+        return zero + bNum;
+    }
+
+
+    public static int findDepth(int length) {
+        int i = 1;
+        while(true) {
+            if((long)Math.pow(2, i) - 1 >= length)
+                return i;
+            i += 1;
+        }
+    }
+}
