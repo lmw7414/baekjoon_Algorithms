07a43bc51491aaf06164e8fbc5bbca89fd9e74b8
이민우 <lmw7414@naver.com>
2023-05-11 15:51:57 +0900

    [Silver II] Title: 스택 수열, Time: 2208 ms, Memory: 138664 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.util.*;
+
+// 스택 수열
+public class Main {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        Queue<Integer> queue = new LinkedList<>();
+        List<Character> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            queue.add(sc.nextInt());
+        }
+        int start = 1;
+        while (!queue.isEmpty()) {
+            int num = queue.peek();
+            if (start <= num) {
+                for (; start <= num; start++) {
+                    stack.push(start);
+                    result.add('+');
+                }
+            }
+            if (stack.peek() > num) {
+                System.out.println("NO");
+                System.exit(0);
+            }
+            while (!stack.isEmpty() && num == stack.peek()) {
+                stack.pop();
+                result.add('-');
+                num = queue.poll();
+            }
+
+        }
+
+
+        for (int i = 0; i < result.size(); i++)
+            System.out.println(result.get(i));
+    }
+
+
+}
