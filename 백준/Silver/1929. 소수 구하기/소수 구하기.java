f2695e50922364d3bad54e0dd06c9233be4fc220
이민우 <lmw7414@naver.com>
2023-05-15 15:28:35 +0900

    [Silver III] Title: 소수 구하기, Time: 960 ms, Memory: 33988 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        boolean[] arr = new boolean[N+1];
+
+        for(int i = 2; i <= N; i++) {
+            if(arr[i])
+                continue;
+            int mod = i;
+            for(int j = 2; i * j <= N; j++) {
+                arr[i * j] = true;
+            }
+        }
+        for(int i = M; i <= N; i++) {
+            if (i < 2)
+                continue;
+            if(!arr[i])
+                System.out.println(i);
+        }
+    }
+}
