6f7677033188ad76b7505336b9355b8b6a6db0fd
이민우 <lmw7414@naver.com>
2023-05-19 14:54:14 +0900

    [Platinum V] Title: LCA 2, Time: 2180 ms, Memory: 181624 KB -BaekjoonHub

--- 백준/Platinum/11438. LCA 2/LCA 2.java
+++ 백준/Platinum/11438. LCA 2/LCA 2.java
@@ -21,34 +21,34 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         tree = new ArrayList[N + 1];
         depth = new int[N + 1];
-        for(int i = 1; i<=N; i++)
+        for (int i = 1; i <= N; i++)
             tree[i] = new ArrayList<>();
 
         int temp = 1;
         kMax = 0;
-        while(temp <= N) {
+        while (temp <= N) {
             temp <<= 1;
             kMax++;
         }
 
-        for(int i = 0; i< N - 1; i++) {
+        for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             tree[a].add(b);
             tree[b].add(a);
         }
-        initTree(N,1);
-        for(int k = 1; k<= kMax; k++) {
-            for(int n = 1; n <= N; n++) {
-                parent[k][n] = parent[k-1][parent[k-1][n]];
+        initTree(N, 1);
+        for (int k = 1; k <= kMax; k++) {
+            for (int n = 1; n <= N; n++) {
+                parent[k][n] = parent[k - 1][parent[k - 1][n]];
             }
         }
 
         st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i< M; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
@@ -59,27 +59,27 @@ public class Main {
 
     private static int fastLCA(int a, int b) {
         // b의 depth가 더 깊게
-        if(depth[a] > depth[b]) {
+        if (depth[a] > depth[b]) {
             int temp = a;
             a = b;
             b = temp;
         }
         // a, b 깊이 맞추기
-        for(int k = kMax; k >=0; k--) {
-            if(Math.pow(2, k) <= depth[b] - depth[a]) {
+        for (int k = kMax; k >= 0; k--) {
+            if (Math.pow(2, k) <= depth[b] - depth[a]) {
                 b = parent[k][b];
 
             }
         }
         // 동시에 올라가면서 조상 찾기
-        for(int k = kMax; k>= 0; k--) {
-            if(parent[k][a] != parent[k][b]) {
+        for (int k = kMax; k >= 0; k--) {
+            if (parent[k][a] != parent[k][b]) {
                 a = parent[k][a];
                 b = parent[k][b];
             }
         }
         int LCA = a;
-        if(a != b) {
+        if (a != b) {
             LCA = parent[0][LCA];
         }
         return LCA;
@@ -93,11 +93,11 @@ public class Main {
         visit[root] = true;
         parent[0][root] = root;
         depth[root] = 0;
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int p = queue.poll();
 
-            for(int leaf : tree[p]) {
-                if(!visit[leaf]) {
+            for (int leaf : tree[p]) {
+                if (!visit[leaf]) {
                     visit[leaf] = true;
                     queue.add(leaf);
                     parent[0][leaf] = p;
@@ -108,5 +108,3 @@ public class Main {
     }
 
 }
-
-
