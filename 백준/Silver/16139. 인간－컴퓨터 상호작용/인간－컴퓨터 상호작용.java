009e7b209d6f30064c4dcb0885f7187e3782d708
이민우 <lmw7414@naver.com>
2023-05-24 14:08:04 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 200 ms, Memory: 16672 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,37 +1,48 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String S = br.readLine();
-        int[][] prefixSum = new int[S.length() + 1][26];
-
-        for (int i = 0; i < S.length(); i++) {
-            int idx = S.charAt(i) - 'a';
-            prefixSum[0][idx]++;
-        }
-        for (int i = 1; i < S.length(); i++) {
-            for (int j = 0; j < 26; j++) {
-                prefixSum[i][j] = prefixSum[i - 1][j];
-            }
-            int idx = S.charAt(i - 1) - 'a';
-            prefixSum[i][idx]--;
-        }
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int tc = Integer.parseInt(st.nextToken());
+        HashMap<Character, List<Integer>> hm = new HashMap<>();
 
+        String S = st.nextToken();
+        for (int i = 0; i < S.length(); i++) {
+            if (!hm.containsKey(S.charAt(i))) {
+                hm.put(S.charAt(i), new ArrayList<>());
+                hm.get(S.charAt(i)).add(i);
+            } else hm.get(S.charAt(i)).add(i);
+        }
+        st = new StringTokenizer(br.readLine());
+        int tc = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= tc; i++) {
             st = new StringTokenizer(br.readLine());
             char A = st.nextToken().charAt(0);
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            System.out.println(prefixSum[start][A - 'a'] - prefixSum[end + 1][A - 'a']);
+            int answer = 0;
+            if (hm.get(A) == null){
+                sb.append(0);
+            sb.append('\n');
+            }
+            else {
+                for (int idx : hm.get(A)) {
+                    if (idx <= end && idx >= start)
+                        answer++;
+                    if (idx > end)
+                        break;
+                }
+                sb.append(answer);
+                sb.append('\n');
+            }
         }
-
+        System.out.println(sb);
     }
 }
