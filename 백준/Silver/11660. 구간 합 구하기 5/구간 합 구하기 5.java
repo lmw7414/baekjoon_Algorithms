b1736ca68614a34b944b7032fe12faaf133b6fe5
이민우 <lmw7414@naver.com>
2023-05-25 15:34:26 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 820 ms, Memory: 118940 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,16 +19,16 @@ class Main {
                 arr[i][j] = arr[i][j - 1] + arr[i - 1][j] - arr[i - 1][j - 1] + Integer.parseInt(st.nextToken());
             }
         }
-
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            int answer = 0;
-            answer = arr[x2][y2] - arr[x2][y1 - 1] - arr[x1-1][y2] + arr[x1 - 1][y1 - 1];
-            System.out.println(answer);
+            sb.append(arr[x2][y2] - arr[x2][y1 - 1] - arr[x1-1][y2] + arr[x1 - 1][y1 - 1]);
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
