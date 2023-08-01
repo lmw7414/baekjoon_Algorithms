d8cf83c12facd8fd6f592bb0511325a43b13ddef
이민우 <lmw7414@naver.com>
2023-08-01 11:09:05 +0900

    [Silver I] Title: 포도주 시식, Time: 152 ms, Memory: 16084 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,37 +1,29 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-class Main {
-    static int[] arr;
-    static int[] dp;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        arr = new int[N + 1];
-        dp = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
+public class Main {
+	static int[] arr;
+	static int[] dp;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N + 1];
+		dp = new int[N + 1];
+		for(int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		dp[1] = arr[1];
+        if(N == 1) {
+            System.out.println(dp[1]);
+            System.exit(0);
         }
+		dp[2] = dp[1] + arr[2];
+		for(int i = 3;  i <= N; i++) {
+			dp[i] = Math.max(Math.max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]), dp[i - 1]);
+		}
+		System.out.println(dp[N]);
+	}
 
-        System.out.println(dp(N));
-    }
-
-    private static int dp(int N) {
-        dp[1] = arr[1];
-        if (N == 1)
-            return dp[1];
-        dp[2] = dp[1] + arr[2];
-        int max = dp[2];
-        for (int i = 3; i <= N; i++) {
-            dp[i] = Math.max(Math.max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]), dp[i - 1]);
-            if (max < dp[i]) max = dp[i];
-        }
-        return max;
-    }
 }
