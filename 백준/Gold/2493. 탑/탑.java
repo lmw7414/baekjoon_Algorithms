b90053abf5a572944ce771a254dc6a98f337e2b4
이민우 <lmw7414@naver.com>
2023-08-07 17:44:13 +0900

    [Gold V] Title: 탑, Time: 3252 ms, Memory: 138524 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N + 1];
+		Stack<int[]> stack = new Stack();
+		Stack<int[]> temp = new Stack();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			stack.push(new int[] {i + 1, Integer.parseInt(st.nextToken())});
+		}
+
+		while(!stack.isEmpty()) {
+			int cnt = 1;
+			while(!temp.isEmpty() && stack.peek()[1] >= temp.peek()[1]) {
+
+				arr[temp.peek()[0]] = stack.size();
+				cnt++;
+				temp.pop();
+			}
+			if(temp.isEmpty() || stack.peek()[1] < temp.peek()[1]) {
+				temp.add(stack.pop());
+			}
+		}
+		for(int i = 1; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
