641eb5efc70186d1b72baa2c3dd16313b5e883be
이민우 <lmw7414@naver.com>
2023-08-08 13:27:40 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 820 ms, Memory: 118940 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,41 +1,34 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N, M;
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         int[][] arr = new int[N + 1][N + 1];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken()) + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
+                arr[i][j] = arr[i][j - 1] + arr[i - 1][j] - arr[i - 1][j - 1] + Integer.parseInt(st.nextToken());
             }
         }
-        
-        StringBuilder sb = new StringBuilder();  // 속도를 올리기 위해 StringBuilder 사용
-        for (int m = 0; m < M; m++) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-
-            sb.append(arr[x2][y2] - arr[x2][y1 - 1] - arr[x1 - 1][y2] + arr[x1 - 1][y1 - 1]);
+            sb.append(arr[x2][y2] - arr[x2][y1 - 1] - arr[x1-1][y2] + arr[x1 - 1][y1 - 1]);
             sb.append("\n");
         }
         System.out.println(sb);
     }
-
 }
