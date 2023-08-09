66513ae49179d11609bd79c3c8e52ece033da116
이민우 <lmw7414@naver.com>
2023-08-09 15:03:06 +0900

    [Silver II] Title: DNA 비밀번호, Time: 312 ms, Memory: 20872 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,88 +1,83 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        int answer = 0;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int strLen = Integer.parseInt(st.nextToken());
-        int partLen = Integer.parseInt(st.nextToken());
-
-        String str = br.readLine();
-        int[] arr = new int[4];  //A, C, G, T
-
-        st = new StringTokenizer(br.readLine());
-
-        for(int i = 0; i< 4; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
-
-
-        int startIdx = 0;
-        int endIdx = partLen;
-        int[] curArr = new int[4];
-
-        for(int i = startIdx; i < endIdx; i++) {
-            addArr(str.charAt(i), curArr);
-        }
-
-        if(isOk(arr, curArr)) {
-            answer++;
-        }
-        while (endIdx < strLen) {
-
-            addArr(str.charAt(endIdx), curArr);
-            endIdx++;
-            subArr(str.charAt(startIdx), curArr);
-            startIdx++;
-            if(isOk(arr, curArr)) {
-                answer++;
-            }
-        }
-        System.out.println(answer);
-    }
-
-    public static void addArr(char A, int[] arr) {
-        switch (A) {
-            case 'A':
-                arr[0]++;
-                break;
-            case  'C':
-                arr[1]++;
-                break;
-            case 'G':
-                arr[2]++;
-                break;
-            case 'T':
-                arr[3]++;
-        }
-    }
-    public static void subArr(char A, int[] arr) {
-        switch (A) {
-            case 'A':
-                arr[0]--;
-                break;
-            case  'C':
-                arr[1]--;
-                break;
-            case 'G':
-                arr[2]--;
-                break;
-            case 'T':
-                arr[3]--;
-        }
-    }
-
-    public static boolean isOk(int[] answer, int[] cur) {
-        for(int i = 0; i< 4; i++) {
-            if(answer[i] > cur[i])
-                return false;
-        }
-        return true;
-    }
+	static int[] answer = new int[4];  // ACGT 갯수를 저장하기 위한 배열
+	static int[] cur = new int[4]; // 주어진 문자열에서 알파벳 카운트하기 위한 배열
+	static int cnt = 0;  // 만들 수 있는 비밀번호의 종류의 수
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int strLen = Integer.parseInt(st.nextToken());  // 주어진 문자열의 길이
+		int partLen = Integer.parseInt(st.nextToken());  // 만들어야 하는 부분문자열의 길이
+		String str = br.readLine();
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < 4; i++) {
+			answer[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int startIdx = 0;  // 시작 문자열
+		int endIdx = partLen;  // 끝 문자열
+		
+		// 부분문자열의 알파벳 개수 미리 탐색 후 다음 탐색 부터는 제거되는 알파벳, 추가되는 알파벳만 감소, 증가
+		for(int i = startIdx; i< endIdx; i++)
+			addChar(str.charAt(i), cur);
+		if(check(answer, cur)) cnt++;
+		
+		// 끝 인덱스가 문자열의 마지막에 도착할 때까지
+		while(endIdx < strLen) {
+			subChar(str.charAt(startIdx++), cur);
+			addChar(str.charAt(endIdx++), cur);
+			if(check(answer, cur)) cnt++;
+		}
+		System.out.println(cnt);
+	}
+	
+	public static boolean check(int[] answer, int[] cur) {
+		for(int i = 0; i< 4; i++) {
+			if(cur[i] < answer[i]) 
+				return false; 
+		}
+		return true;
+	}
+	
+	public static void addChar(char C , int[] arr) {
+		switch(C) {
+		case 'A':
+			arr[0]++;
+			break;
+		case 'C':
+			arr[1]++;
+			break;
+		case 'G':
+			arr[2]++;
+			break;
+		case 'T':
+			arr[3]++;
+			break;
+		}
+	}
+	
+	public static void subChar(char C , int[] arr) {
+		switch(C) {
+		case 'A':
+			arr[0]--;
+			break;
+		case 'C':
+			arr[1]--;
+			break;
+		case 'G':
+			arr[2]--;
+			break;
+		case 'T':
+			arr[3]--;
+			break;
+		}
+	}
+	
 }
