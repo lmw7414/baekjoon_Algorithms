cbedd6722f823c5e8220e66fd5d4b7c01a401e84
이민우 <lmw7414@naver.com>
2023-08-09 15:12:22 +0900

    [Silver I] Title: 절댓값 힙, Time: 328 ms, Memory: 25176 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,29 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.Scanner;
 
+/**
+ * 1. 우선순위 큐를 사용
+ * 2. Comparator 오버라이딩 후 비교 조건 정하기
+ * 3. 절댓값이 같다면 작은 수를 저장
+ * 4. 절댓값이 작으면 그냥 저장
+ */
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> {
-            if(Math.abs(a) == Math.abs(b))
-                return a - b;
-            return Math.abs(a) - Math.abs(b);
-        });
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>((a1, b1) -> {
+			if(Math.abs(a1) == Math.abs(b1))
+					return a1 - b1;
+			return Math.abs(a1) - Math.abs(b1);
+		});
+		
+		for(int i = 0; i< N; i++) {
+			int input = Integer.parseInt(br.readLine());
+			if(input == 0) {
+				if(pq.isEmpty()) sb.append(0).append("\n");
+				else sb.append(pq.poll()).append("\n");
+			} else pq.add(input);
+		}
+		System.out.print(sb);
+	}
 
-        int N = sc.nextInt();
-
-        for(int i = 0; i< N; i++) {
-            int input = sc.nextInt();
-
-            if(input == 0) {
-                if(pq.isEmpty())
-                    System.out.println(0);
-                else
-                    System.out.println(pq.poll());
-            }else
-                pq.add(input);
-
-        }
-    }
 }
