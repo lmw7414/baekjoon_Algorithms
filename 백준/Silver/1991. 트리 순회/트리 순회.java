7d0f606eb00690d0336fef3fdb8cd658added5d5
Minwoo Lee <lmw7414@naver.com>
2023-08-14 17:24:21 +0900

    [Silver I] Title: 트리 순회, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,57 +1,60 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static HashMap<Character, ArrayList<Character>> hm;
+	static HashMap<Character, ArrayList<Character>> hm = new HashMap<>();
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			char parent = st.nextToken().charAt(0);
+			char child1 = st.nextToken().charAt(0);
+			char child2 = st.nextToken().charAt(0);
+			
+			hm.put(parent, new ArrayList<>());
+			hm.get(parent).add(child1);
+			hm.get(parent).add(child2);
+			
+		}
+		preorderTraversal('A');
+		sb.append("\n");
+		inorderTraversal('A');
+		sb.append("\n");
+		postorderTraversal('A');
+		System.out.println(sb);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        hm = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            char root = st.nextToken().charAt(0);
-            char left = st.nextToken().charAt(0);
-            char right = st.nextToken().charAt(0);
-
-            hm.put(root, new ArrayList<>(List.of(left, right)));
-        }
-        preorder('A');
-        System.out.println();
-        inorder('A');
-        System.out.println();
-        postorder('A');
-
-    }
-
-    public static void preorder(char root) {
-        if (root == '.') return;
-        System.out.print(root);
-        preorder(hm.get(root).get(0));
-        preorder(hm.get(root).get(1));
-    }
-
-    public static void inorder(char root) {
-        if (root == '.') return;
-        inorder(hm.get(root).get(0));
-        System.out.print(root);
-        inorder(hm.get(root).get(1));
-    }
-
-    public static void postorder(char root) {
-        if (root == '.') return;
-        postorder(hm.get(root).get(0));
-        postorder(hm.get(root).get(1));
-        System.out.print(root);
-    }
+		
+	}
+	//전위
+	public static void preorderTraversal(char root) {
+		if(root == '.') return;
+		sb.append(root);
+		preorderTraversal(hm.get(root).get(0));
+		preorderTraversal(hm.get(root).get(1));
+	
+	}
+	//중위
+	public static void inorderTraversal(char root) {
+		if(root == '.') return;
+		inorderTraversal(hm.get(root).get(0));
+		sb.append(root);
+		inorderTraversal(hm.get(root).get(1));
+		
+	}
+	//후위
+	public static void postorderTraversal(char root) {
+		if(root == '.') return;
+		postorderTraversal(hm.get(root).get(0));
+		postorderTraversal(hm.get(root).get(1));
+		sb.append(root);
+	}
+	
 }
-
-
-
