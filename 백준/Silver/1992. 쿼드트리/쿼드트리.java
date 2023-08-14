bda11ee1b82401e131c23ad0fc5c0425187758bf
Minwoo Lee <lmw7414@naver.com>
2023-08-14 16:59:45 +0900

    [Silver I] Title: 쿼드트리, Time: 132 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -3,55 +3,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int[][] arr;
+	static int[][] arr;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		for(int i = 0; i< N; i++) {
+			String str = br.readLine();
+			for(int j = 0; j < N; j++) {
+				arr[i][j] = str.charAt(j) - '0';
+			}
+		}
+		calc(0, 0, N);
+		System.out.print(sb);
+	}
+	// 왼쪽 상단 -> 오른쪽 상단 -> 왼쪽 하단 -> 오른쪽 하단
+	public static void calc(int x, int y, int N) {
+		
+		int flag = arr[x][y];
+		boolean check = true;
+		for(int i = x; i < x + N; i++) {
+			for(int j = y; j < y + N; j++) {
+				if(arr[i][j] != flag) {
+					check = false;
+					break;
+				}
+			}
+			if(!check) break;
+		}
+		
+		if(check) {
+			sb.append(flag);
+		}else {
+			sb.append("(");
+			int half = N / 2;
+			calc(x, y, half);
+			calc(x, y + half, half);
+			calc(x + half, y, half);
+			calc(x + half, y + half, half);
+			sb.append(")");
+		}
+		
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String inputSize = br.readLine();
-        int size = Integer.parseInt(inputSize);
-
-        arr = new int[size + 1][size + 1];
-        for (int i = 1; i <= size; i++) {
-            String arg = br.readLine();
-            for (int j = 1; j <= size; j++) {
-                arr[i][j] = arg.charAt(j - 1) - '0';
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        divAndCon(sb, 1, 1, size, size);
-        System.out.println(sb.toString());
-    }
-
-    public static void divAndCon(StringBuilder sb, int startX, int startY, int endX, int endY) {
-        int midX = (startX + endX) / 2;
-        int midY = (startY + endY) / 2;
-
-        if (startX >= endX || startY >= endY) {
-            sb.append(arr[startX][startY]);
-            return;
-        }
-        int atom = arr[startX][startY];
-        boolean flag = true;
-        for (int i = startX; i <= endX; i++) {
-            for (int j = startY; j <= endY; j++) {
-                if (atom != arr[i][j]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (!flag) {
-                break;
-            }
-        }
-        if (!flag) {
-            sb.append('(');
-            divAndCon(sb, startX, startY, midX, midY);  //1
-            divAndCon(sb, startX, midY + 1, midX, endY);  //2
-            divAndCon(sb, midX + 1, startY, endX, midY);  //3
-            divAndCon(sb, midX + 1, midY + 1, endX, endY);  //4
-            sb.append(')');
-        } else {
-            sb.append(atom);
-        }
-    }
 }
