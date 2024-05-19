9aa0b8ab2740b1965a6fc16f7e97e56bf8a4be5b
Minwoo Lee <lmw7414@naver.com>
2024-05-19 20:28:54 +0900

    [Gold III] Title: 괄호 추가하기, Time: 124 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -1,44 +1,81 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-// 정답 보고 풀이
+
 public class Main {
-    static int answer = Integer.MIN_VALUE;
-    static int N;
-    static char[] cArr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        cArr = new char[N];
-        cArr = br.readLine().toCharArray();
-        dfs(2, cArr[0] - '0');
-        System.out.println(answer);
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String nStr = br.readLine();
+            int n = Integer.parseInt(nStr);
+            char[] buff = new char[n];
+            br.read(buff, 0, n);
+
+            int[] numArr = new int[20];
+            char[] opArr = new char[20];
+            int[] dpMax = new int[30];
+            int[] dpMin = new int[30];
+
+            for (int i = 0; i < n; i++) {
+                if (i % 2 == 0) {
+                    numArr[i / 2] = buff[i] - '0';
+                } else {
+                    opArr[i / 2] = buff[i];
+                }
+            }
+
+            dpMax[0] = numArr[0];
+            dpMin[0] = numArr[0];
+            if (n == 1) {
+                System.out.println(dpMax[0]);
+                return;
+            }
+            dpMax[1] = opFunc(numArr[0], numArr[1], opArr[0]);
+            dpMin[1] = dpMax[1];
+
+            for (int i = 2; i < n / 2 + 1; i++) {
+                dpMax[i] = Math.max(
+                        opFunc(dpMax[i - 1], numArr[i], opArr[i - 1]),
+                        opFunc(dpMax[i - 2], opFunc(numArr[i - 1], numArr[i], opArr[i - 1]), opArr[i - 2])
+                );
+                dpMax[i] = Math.max(
+                        dpMax[i],
+                        opFunc(dpMin[i - 1], numArr[i], opArr[i - 1])
+                );
+                dpMax[i] = Math.max(
+                        dpMax[i],
+                        opFunc(dpMin[i - 2], opFunc(numArr[i - 1], numArr[i], opArr[i - 1]), opArr[i - 2])
+                );
+
+                dpMin[i] = Math.min(
+                        opFunc(dpMin[i - 1], numArr[i], opArr[i - 1]),
+                        opFunc(dpMin[i - 2], opFunc(numArr[i - 1], numArr[i], opArr[i - 1]), opArr[i - 2])
+                );
+                dpMin[i] = Math.min(
+                        dpMin[i],
+                        opFunc(dpMax[i - 1], numArr[i], opArr[i - 1])
+                );
+                dpMin[i] = Math.min(
+                        dpMin[i],
+                        opFunc(dpMax[i - 2], opFunc(numArr[i - 1], numArr[i], opArr[i - 1]), opArr[i - 2])
+                );
+            }
+
+            System.out.println(dpMax[n / 2]);
+        } catch (IOException e) {
+
+        }
     }
 
-    public static void dfs(int idx, int sum) {
-        if(idx >= N) {
-            answer = Math.max(sum, answer);
-            return;
-        }
-        // 괄호가 쳐져있지 않은 경우
-        dfs(idx + 2, calc(sum, cArr[idx] - '0', cArr[idx - 1]));
-
-        if(idx + 2 < N) {
-            int right = calc(cArr[idx] - '0' , cArr[idx + 2] - '0', cArr[idx + 1]);
-            int left = calc(sum, right, cArr[idx - 1]);
-            dfs(idx + 4, left);
-        }
-    }
-
-    static int calc(int a, int b, char op) {
+    static int opFunc(int a, int b, char op) {
         switch (op) {
             case '+':
                 return a + b;
-            case '-':
-                return a - b;
             case '*':
                 return a * b;
+            case '-':
+                return a - b;
+            default:
+                return 0;
         }
-        return 0;
     }
 }
