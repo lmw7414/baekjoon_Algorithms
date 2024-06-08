19ae627c25b3bbf0143963b0b08bfa49796ac1e2
Minwoo Lee <lmw7414@naver.com>
2024-06-08 16:28:48 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 196 ms, Memory: 16272 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,7 +21,10 @@ public class Main {
             arr[i + 1] = Integer.parseInt(st.nextToken());
         }
         dp = new int[N][N];
-        // Bottom-Up 방식
+        System.out.println(bottom_up());
+
+    }
+    public static int bottom_up() {
         for (int i = 2; i < N + 1; i++) {
             for (int j = 0; j < N - i + 1; j++) { // 시작 지점부터 N에서 i 뺀 만큼
                 dp[j][j + i - 1] = INF;
@@ -31,8 +35,31 @@ public class Main {
                 }
             }
         }
-        System.out.println(dp[0][N - 1]);
-        
+        return dp[0][N - 1];
+    }
+
+    public static int top_down(boolean isFirst, int pos, int cur) {
+        if(isFirst) {
+            for(int i = 0; i< N; i++) {
+                Arrays.fill(dp[i], INF);
+            }
+        }
+        if(pos == cur) return 0;
+        if(dp[pos][cur] != INF) return dp[pos][cur];
+
+        for(int i = pos; i < cur; i++) {
+            int value = top_down(false, pos, i) + top_down(false,i + 1, cur) + (arr[pos] * arr[i + 1] * arr[cur + 1]);
+            dp[pos][cur] = Math.min(value, dp[pos][cur]);
+        }
+        return dp[pos][cur];
+    }
+
+    public static void printArr() {
+        for(int i = 0; i< N; i++) {
+            for (int j = 0; j< N; j++)
+                System.out.print(dp[i][j] + "\t");
+            System.out.println();
+        }
     }
 
 
