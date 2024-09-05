7ae04ee7f1ab6d5c3938ccf85601b141c13c18f8
Minwoo Lee <lmw7414@naver.com>
2024-09-06 04:05:13 +0900

    Update 앱.java

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -5,49 +5,41 @@ import java.util.StringTokenizer;
 
 // 백준 7579번: 앱
 public class Main {
-
-    static int N, M;
-    static int[] m;
-    static int[] c;
+static int N, M;
+    static int[] memories, costs;
     static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+        memories = new int[N];
+        costs = new int[N];
+        dp = new int[N][10001]; // 앱번호, 비용, dp[앱번호][비용] = 최소 사용 메모리
         M = Integer.parseInt(st.nextToken());
-        m = new int[N];
-        c = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            m[i] = Integer.parseInt(st.nextToken());
+            memories[i] = Integer.parseInt(st.nextToken());
         }
-        st =  new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            c[i] = Integer.parseInt(st.nextToken());
+            costs[i] = Integer.parseInt(st.nextToken());
         }
-        dp = new int[N][10001];
 
         int answer = Integer.MAX_VALUE;
-        for (int i = 0; i < N; i++) {
-            int cost = c[i];
-            int memory = m[i];
+        for (int j = costs[0]; j < 10001; j++) {
+            dp[0][j] = memories[0];
+            if (M <= dp[0][j]) answer = Math.min(answer, j);
+        }
 
-            for (int j = 0; j <= 10000; j++) {
-                if (i == 0) {
-                    if (j >= cost) dp[i][j] = memory;
-                } else {
-                    if (j >= cost) {
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost] + memory);
-                    } else {
-                        dp[i][j] = dp[i - 1][j];
-                    }
-                }
-                if (dp[i][j] >= M) {
-                    answer = Math.min(answer, j);
-                }
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < 10001; j++) {
+                if (j < costs[i]) dp[i][j] = dp[i - 1][j];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - costs[i]] + memories[i]);
+                if (M <= dp[i][j]) answer = Math.min(answer, j);
             }
         }
         System.out.println(answer);
     }
+
 }
