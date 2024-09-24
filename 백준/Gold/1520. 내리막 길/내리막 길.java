e9b18c121ec88136f39e9c162b59cb3a952ae75b
Minwoo Lee <lmw7414@naver.com>
2024-09-25 00:23:04 +0900

    [Gold III] Title: 내리막 길, Time: 364 ms, Memory: 41672 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,59 +1,81 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     static int M, N;
-    static int[][] data, dp;
-    static int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    static int[][] cnt;
+    static Node[][] arr;
+    static boolean[][] visit;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        data = new int[M][];
-        int count = 0;
-
+        arr = new Node[M][N];
+        cnt = new int[M][N];
+        visit = new boolean[M][N];
         for (int i = 0; i < M; i++) {
-            data[i] = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt).toArray();
-        }
-
-        dp = new int[M][N];
-        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                dp[i][j] = -1;
+                arr[i][j] = new Node(i, j, Integer.parseInt(st.nextToken()));
             }
         }
-
-        System.out.println(dfs(0, 0));
+        calc(0, 0);
+//        printArr();
+        System.out.println(cnt[M - 1][N - 1]);
     }
 
-    static int dfs(int x, int y) {
-        if (x == M-1 && y == N-1) {
-            return 1;
-        }
 
-        // 이미 탐색된 값인 경우
-        if (dp[x][y] != -1) {
-            return dp[x][y];
-        }
+    public static void calc(int x, int y) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        cnt[x][y] = 1;
+        pq.add(arr[x][y]);
 
-        dp[x][y] = 0;  // 한번도 방문하지 않은 위치에 대해서는 현재 위치에서 끝점까지 도달하는 경우의 수를 0으로 초기화
-        for (int[] dir : dirs) {
-            int newX = x + dir[0];
-            int newY = y + dir[1];
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if(visit[cur.x][cur.y]) continue;
+//            System.out.printf("%d %d\n", cur.x, cur.y);
+//            System.out.printf("%d\n", arr[cur.x][cur.y].val);
+            visit[cur.x][cur.y] = true;
+            for (int d = 0; d < 4; d++) {
+                int nx = cur.x + dx[d];
+                int ny = cur.y + dy[d];
 
-            if (newX >= 0 && newY >= 0 && newX <= M-1 && newY <= N-1) {
-                if (data[newX][newY] < data[x][y]) {
-                    dp[x][y] += dfs(newX, newY);
-                }
+                if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
+                if (arr[nx][ny].val >= arr[cur.x][cur.y].val) continue;
+                if (visit[nx][ny]) continue;
+
+                //if (cnt[nx][ny] == 0) cnt[nx][ny] = cnt[cur.x][cur.y];
+                cnt[nx][ny] += cnt[cur.x][cur.y];
+                pq.add(arr[nx][ny]);
             }
         }
+    }
 
-        return dp[x][y];
+    public static void printArr() {
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(cnt[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    static class Node implements Comparable<Node> {
+        int x, y, val;
+
+        public Node(int x, int y, int val) {
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return o.val - this.val;
+        }
     }
 }
