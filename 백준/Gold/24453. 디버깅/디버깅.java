91e6fe48981dedcb23810cce77cf781abe7b2fdb
Minwoo Lee <lmw7414@naver.com>
2024-10-14 18:27:55 +0900

    [Gold IV] Title: 디버깅, Time: 516 ms, Memory: 77660 KB -BaekjoonHub

---
+++ 백준/Gold/24453. 디버깅/디버깅.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M, X, Y;
+    static int[] arr;
+    static int answer = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        X = Integer.parseInt(st.nextToken());
+        Y = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i < M; i++) {
+            if (arr[i] - arr[i - 1] - 1 >= X) {
+                System.out.println(M - Y);
+                System.exit(0);
+            }
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        int errIdx = 0;
+        for (int i = 0; i < M; i++) {
+            if (arr[i] > 0 && arr[i] <= X) {
+                queue.add(arr[i]);
+                errIdx = i;
+            } else break;
+        }
+        if (queue.size() >= Y) answer = Math.max(answer, M - queue.size());
+
+        int left = 2;
+        int right = X + 1;
+        for (; right <= N; right++) {
+            if (!queue.isEmpty() && queue.peek() < left) queue.poll();
+            if (errIdx + 1 < arr.length && arr[errIdx + 1] <= right) queue.add(arr[++errIdx]);
+            left++;
+            answer = Math.max(answer, M - Math.max(queue.size(), Y));
+        }
+        System.out.println(answer);
+    }
+}
