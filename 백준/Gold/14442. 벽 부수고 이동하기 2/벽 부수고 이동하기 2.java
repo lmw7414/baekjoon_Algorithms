88dca0524708f2cf52912352283a0739e4ccd91a
Minwoo Lee <lmw7414@naver.com>
2024-11-19 18:43:14 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1428 ms, Memory: 337776 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M, K;
-    static int INF = 7654321;
     static int[][] arr;
-    static int[][][] dp;
+    static boolean[][][] visit;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
@@ -18,15 +19,7 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         arr = new int[N + 1][M + 1];
-        dp = new int[N + 1][M + 1][K + 1];
-
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                for (int k = 0; k <= K; k++) {
-                    dp[i][j][k] = INF;
-                }
-            }
-        }
+        visit = new boolean[N + 1][M + 1][K + 1];
 
         for (int i = 1; i <= N; i++) {
             String str = br.readLine();
@@ -34,41 +27,34 @@ public class Main {
                 arr[i][j] = str.charAt(j - 1) - '0';
             }
         }
-
-        BFS(1, 1, 0);
-        int answer = Integer.MAX_VALUE;
-        for (int k = 0; k <= K; k++) answer = Math.min(answer, dp[N][M][k]);
-        if (answer == INF) System.out.println(-1);
-        else System.out.println(answer);
+        System.out.println(BFS(1, 1, 0));
     }
 
-    public static void BFS(int x, int y, int k) {
+    public static int BFS(int x, int y, int k) {
         Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{x, y, k});
-        dp[x][y][k] = 1;
-        while(!queue.isEmpty()) {
+        queue.add(new int[]{x, y, k, 1});
+        visit[x][y][k] = true;
+        while (!queue.isEmpty()) {
             int[] cur = queue.poll();
+            if (cur[0] == N && cur[1] == M) return cur[3];
 
-            for(int d = 0; d < 4; d++) {
+            for (int d = 0; d < 4; d++) {
                 int nx = cur[0] + dx[d];
                 int ny = cur[1] + dy[d];
                 if (nx <= 0 || ny <= 0 || nx > N || ny > M) continue;
-
+                if (visit[nx][ny][cur[2]]) continue;  // 이미 방문했으면
                 if (arr[nx][ny] == 1) { // 부수고 가거나, 다른 길로 가거나
                     if (cur[2] < K) {
-                        if (dp[nx][ny][cur[2] + 1] > dp[cur[0]][cur[1]][cur[2]] + 1) {
-                            dp[nx][ny][cur[2] + 1] = dp[cur[0]][cur[1]][cur[2]] + 1;
-                            queue.add(new int[]{nx, ny, cur[2] + 1});
-                        }
+                        visit[nx][ny][cur[2]] = true;
+                        queue.add(new int[]{nx, ny, cur[2] + 1, cur[3] + 1});
                     }
                 } else {
-                    if (dp[nx][ny][cur[2]] > dp[cur[0]][cur[1]][cur[2]] + 1) {
-                        dp[nx][ny][cur[2]] = dp[cur[0]][cur[1]][cur[2]] + 1;
-                        queue.add(new int[]{nx, ny, cur[2]});
-                    }
+                    visit[nx][ny][cur[2]] = true;
+                    queue.add(new int[]{nx, ny, cur[2], cur[3] + 1});
                 }
             }
         }
+        return -1;
     }
 
 }
