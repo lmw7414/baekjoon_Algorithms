328558b0c29f342c3e98196d4b0f1796204a109b
Minwoo Lee <lmw7414@naver.com>
2024-12-24 17:18:38 +0900

    [Gold III] Title: 골목 대장 호석 - 효율성 1, Time: 1556 ms, Memory: 220832 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.java
+++ 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,8 +8,7 @@ public class Main {
     static int N, M, A, B, C;
     static List<Node>[] adjList;
     static int pride = Integer.MAX_VALUE; // 수치심
-    static boolean[] visit;
-
+    static int[] dist;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,9 +17,12 @@ public class Main {
         A = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        visit = new boolean[N + 1];
+        dist = new int[N + 1];
+
         adjList = new List[N + 1];
-        for (int i = 1; i <= N; i++) adjList[i] = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
 
         for (int m = 0; m < M; m++) {
             st = new StringTokenizer(br.readLine());
@@ -31,32 +34,52 @@ public class Main {
             adjList[from].add(new Node(to, weight));
             adjList[to].add(new Node(from, weight));
         }
-        for(int i = 1; i <= N; i++) adjList[i].sort((a, b) -> a.w - b.w);
 
-        calc(A, 0, 0);
+        BS();
         if(pride != Integer.MAX_VALUE) System.out.println(pride);
         else System.out.println(-1);
     }
 
+    public static void BS() {
+        int left = 1;
+        int right = 21;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(dijkstra(mid)) { // -> 수치심을 낮춰야함
+                pride = mid;
+                right = mid - 1;
+            } else { // -> 수치심을 높여야함
+                left = mid + 1;
+            }
+        }
+    }
 
-    // 최대 비용이 최소가 되도록
-    public static void calc(int cur, int maxCost, int curCost) {
-        if (curCost > C) return;
-        if(pride < maxCost) return;
-        if (cur == B) {
-            pride = maxCost;
-            return;
+    // cost : 내가 생각하는 최소한의 수치심
+    public static boolean dijkstra(int minPride) {
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        PriorityQueue<Node> pq = new PriorityQueue<>((a1, b1) -> a1.w - b1.w);
+        dist[A] = 0;
+        pq.add(new Node(A, 0));
+
+        while(!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if(cur.v == B) return dist[cur.v] <= C;
+            if(dist[cur.v] < cur.w) continue;
+
+            for(Node next : adjList[cur.v]) {
+                if(next.w > minPride) continue;
+                int total = dist[cur.v] + next.w;
+                if(total > C) continue;
+                if(dist[next.v] < total) continue;
+                dist[next.v] = total;
+                pq.add(new Node(next.v, total));
+            }
         }
-        visit[cur] = true;
-        for (Node next : adjList[cur]) {
-            if (visit[next.v]) continue;
-            calc(next.v, Math.max(maxCost, next.w), curCost + next.w);
-        }
+        return false;
     }
 
     static class Node {
         int v, w;
-
         public Node(int v, int w) {
             this.v = v;
             this.w = w;
