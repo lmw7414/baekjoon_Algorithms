5c7eaa9a423920756b07f7f8876467198ced305c
Minwoo Lee <lmw7414@naver.com>
2025-01-01 02:35:44 +0900

    [level 3] Title: 등대, Time: 55.33 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -1,39 +1,28 @@
-/*
-[문제 설명]
-1. 1~n까지 서로 다른 번호가 매겨진 등대가 있다.
-2. 등대와 등대 사이를 오가는 뱃길이 n-1개 존재
-3. 어느 등대에서 출발해도 다른 모드 등대까지 이동할 수 있음
-4. 전력을 아끼기 위해 이 중 몇개의 등대만 켜두려고 함
-5. 전력을 아낄 수 있도록 등대의 개수를 최소화 해라
-[제한 사항]
-2 ≤ n ≤ 100,000
-
-[문제 해결 프로세스]
-1. 한 뱃길의 양쪽 끝 등대 중 적어도 하나는 켜져 있어야 함.
-2. degree가 1인 등대를 높은 우선순위로 큐에 넣음
-3. 다음 등대는 무조건 불을 킴
-4. 불 킨 등대의 모든 경로 제거
-*/
 import java.util.*;
+/*
+N: 최대 10만개의 등대
+Edge: N-1
+한 뱃길의 양쪽 끝 등대 중 적어도 하나는 켜져 있도록
+
+1. degree 1인 노드 탐색
+*/
 class Solution {
     static int answer = 0;
-    static Set<Integer>[] lhList;
     static int[] degree;
+    static List<Integer>[] adjList;
     public int solution(int n, int[][] lighthouse) {
         degree = new int[n + 1];
-        lhList = new Set[n + 1];
-        for(int i = 1; i<= n; i++) {
-            lhList[i] = new HashSet<>();
+        adjList = new List[n + 1];
+        for(int i = 1; i <= n; i++) adjList[i] = new ArrayList<>();
+        for(int[] edge: lighthouse) {
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+            adjList[edge[0]].add(edge[1]);
+            adjList[edge[1]].add(edge[0]);
         }
-        for(int[] line : lighthouse) {
-            lhList[line[0]].add(line[1]);
-            lhList[line[1]].add(line[0]);
-            degree[line[0]]++;
-            degree[line[1]]++;
-        }
-        calc(n);
+    
+        calc(n);        
         return answer;
-        
     }
     
     public void calc(int n) {
@@ -41,27 +30,20 @@ class Solution {
         while(!flag) {
             flag = true;
             for(int i = 1; i <= n; i++) {
-                if(degree[i] == 1) {
-                    Iterator<Integer> it = lhList[i].iterator();
-                    flag = false;
-                    deleteRoute(it.next());
-                }
+                if(degree[i] != 1) continue;
+                flag = false;
+                delete(adjList[i].get(0));
             }
         }
-        
     }
-    
-    public void deleteRoute(int num) {
-        for(int next : lhList[num]) {
-            lhList[next].remove(num);
-            degree[next]--;
+    public void delete(int node) {
+        for(int child : adjList[node]) {
+            degree[child]--;
+            degree[node]--;
+            adjList[child].remove(Integer.valueOf(node));
         }
-        lhList[num] = null;
-        degree[num] = 0;
+        adjList[node] = null;
         answer++;
     }
-    
-    
-    
     
 }
