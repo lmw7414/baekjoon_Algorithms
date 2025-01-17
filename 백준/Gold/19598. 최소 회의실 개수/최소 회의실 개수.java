2da293e62007d2279f6c87e9a2e8512c8130a0fc
Minwoo Lee <lmw7414@naver.com>
2025-01-17 14:49:55 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 568 ms, Memory: 52060 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -15,46 +14,30 @@ import java.util.StringTokenizer;
  */
 public class Main {
     static int N;
-    static Time[] times;
+    static PriorityQueue<Time> times;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        times = new Time[N];
+        times = new PriorityQueue<>();
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            times[i] = new Time(s, e);
+            times.add(new Time(s, e));
         }
-        Arrays.sort(times);
-
-        int left = 1;
-        int right = N + 1;
-        int answer = N;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (calc(mid)) {
-                answer = mid;
-                right = mid - 1;
-            } else left = mid + 1;
-        }
-        System.out.println(answer);
-    }
-
-    public static boolean calc(int key) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < key; i++) pq.add(0);
-
-        for (int i = 0; i < N; i++) {
-            Time t = times[i];
-            if (pq.isEmpty()) return false;
-            int end = pq.poll();
-            if (end <= t.s) pq.add(t.e);
-            else return false;
+        while (!times.isEmpty()) {
+            Time t = times.poll();
+            if (pq.isEmpty() || pq.peek() > t.s) pq.add(t.e);
+            else {
+                pq.poll();
+                pq.add(t.e);
+            }
         }
-        return true;
+        System.out.println(pq.size());
     }
+
 
     static class Time implements Comparable<Time> {
         int s, e;
@@ -63,6 +46,7 @@ public class Main {
             this.s = s;
             this.e = e;
         }
+
         @Override
         public int compareTo(Time o) {
             if (this.s == o.s) return this.e - o.e;
