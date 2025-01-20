2d3831d04a08f471fdc8124699eb97f42a17a1c7
Minwoo Lee <lmw7414@naver.com>
2025-01-21 02:53:35 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 464 ms, Memory: 37128 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,58 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-//욕심쟁이 판다
+/**
+ * dp[i][j] : 현재 지점에서부터 갈 수 있는 최대 거리
+ */
 public class Main {
-
     static int N;
-    static int MAX = 0;
-    static int board[][];
-    static int dp[][];
-    static boolean visited[][];
-
+    static int[][] arr, dp;
+    static int[] dx = {-1, 1, 0 ,0};
+    static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-
-        board = new int[N][N];
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
         dp = new int[N][N];
-        visited = new boolean[N][N];
 
-        for(int i= 0; i< N; i++) {
+        StringTokenizer st;
+        for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j< N; j++)
-                board[i][j] = Integer.parseInt(st.nextToken());
-        }
-
-        for(int i= 0 ; i< N; i++ ) {
-            for(int j = 0; j< N; j++) {
-                MAX = Math.max(DFS(i, j), MAX);
+            for(int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        System.out.println(MAX);
+        int answer = 0;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                dp[i][j] = DFS(i, j);
+                answer = Math.max(answer, dp[i][j]);
+            }
+        }
+        System.out.println(answer);
     }
 
-    static int DFS(int x, int y) {
-        int dx[] = {0, 1, 0, -1};
-        int dy[] = {-1, 0, 1, 0};
-
-        if (dp[x][y] != 0)
-            return dp[x][y];
-
+    public static int DFS(int x, int y) {
+        if(dp[x][y] != 0) return dp[x][y];
         dp[x][y] = 1;
-
-        for (int i = 0; i < 4; i++) {
-            int cx = x + dx[i];
-            int cy = y + dy[i];
-
-            if (cx >= 0 && cx < N && cy >= 0 && cy < N)
-                if (board[x][y] < board[cx][cy])
-                    dp[x][y] = Math.max(dp[x][y], DFS(cx, cy) + 1);
+        for(int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if(OOB(nx, ny)) continue;
+            if(arr[x][y] >= arr[nx][ny]) continue;
+            dp[x][y] = Math.max(dp[x][y], DFS(nx, ny) + 1);
         }
         return dp[x][y];
     }
+
+    public static boolean OOB(int x, int y) {
+        return x < 0 || y < 0 || x >= N || y >= N;
+    }
+
 }
