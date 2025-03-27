1d239279ac6b70a8e3057fbb7279dd3e12429a6f
Minwoo Lee <lmw7414@naver.com>
2025-03-28 00:37:03 +0900

    [Gold I] Title: 수업, Time: 1292 ms, Memory: 143964 KB -BaekjoonHub

--- 백준/Gold/19700. 수업/수업.java
+++ 백준/Gold/19700. 수업/수업.java
@@ -1,61 +1,66 @@
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 /**
  * [주의]
  * 학생들의 키는 모두 다름
  * [문제 해결 프로세스]
- 1. 키 내림 차순 정렬
- 2. 내가 들어갈 수 있는 팀 중에서 최대 인원인 팀에 들어가는 것이 좋음
+ * 1. 키 내림 차순 정렬
+ * 2. 내가 들어갈 수 있는 팀 중에서 최대 인원인 팀에 들어가는 것이 좋음
  */
 public class Main {
     static int N;
-    static Oj[] arr;
-    static int answer = 0;
+    static List<Integer> teams = new ArrayList<>();
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        arr = new Oj[N];
-
+        PriorityQueue<Oj> pq = new PriorityQueue<>((a, b) -> b.height - a.height);
         StringTokenizer st;
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int height = Integer.parseInt(st.nextToken());
             int before = Integer.parseInt(st.nextToken());
-            arr[i] = new Oj(height, before);
+            pq.add(new Oj(height, before));
         }
-        Arrays.sort(arr, (a, b) -> b.height - a.height);
-        TreeSet<Integer> ts = new TreeSet<>();
-        int[] teams = new int[N + 1]; // 최대 팀의 수 -> N + 1
-        for(Oj o : arr) {
-            int k = o.before;
-            Integer team = ts.lower(k);
-            if(team == null) {
-                ts.add(1);
-                teams[1]++;
+
+        while (!pq.isEmpty()) {
+            int idx = getLowerBoundIdx(pq.poll().before);
+            if(idx == -1) {
+                teams.add(0, 1);
             } else {
-                ts.add(team + 1);
-                teams[team + 1]++;
-                teams[team]--;
-                if(teams[team] == 0) ts.remove(team);
+                teams.set(idx, teams.get(idx) + 1);
             }
         }
 
-        for(int i = 1; i<= N; i++) {
-            //System.out.println(teams[i]);
-            if(teams[i] != 0) answer+= teams[i];
+        System.out.println(teams.size());
+    }
+
+    public static int getLowerBoundIdx(int key) {
+        int left = 0;
+        int right = teams.size() - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (teams.get(mid) < key) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        System.out.println(answer);
+        return result;
     }
 
     static class Oj {
         int height, before;
+
         public Oj(int height, int before) {
             this.height = height;
             this.before = before;
         }
     }
-
-
+    
 }
