92d45a8bc832958b59740ecf2788fcd2254afef1
Minwoo Lee <lmw7414@naver.com>
2025-05-22 17:34:11 +0900

    [Gold II] Title: 줄서기, Time: 664 ms, Memory: 216536 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14864. 줄서기/줄서기.java
+++ 백준/Gold/14864. 줄서기/줄서기.java
@@ -8,8 +8,6 @@ import java.util.*;
 public class Main {
     static int N, M;
     static int[] arr;
-    static Map<Integer, Set<Integer>> out = new HashMap<>(), in = new HashMap<>();
-    static int[] degree;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,59 +15,29 @@ public class Main {
         N = Integer.parseInt(st.nextToken()); // 최대 십만
         M = Integer.parseInt(st.nextToken()); // 최대 백만
         arr = new int[N + 1];
-        degree = new int[N + 1];
         for (int i = 1; i <= N; i++) {
-            out.put(i, new HashSet<>());
-            in.put(i, new HashSet<>());
+            arr[i] = i;
         }
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            out.get(s).add(e);
-            in.get(e).add(s);
-            degree[s]++;
+            arr[s]++;
+            arr[e]--;
         }
-        calc();
-        if (isCorrect()) {
-            for (int i = 1; i <= N; i++) {
-                System.out.print(arr[i] + " ");
+        Set<Integer> set = new HashSet<>();
+        for(int i = 1; i<= N; i++) {
+            if(set.contains(arr[i])) {
+                System.out.println(-1);
+                return;
             }
-        } else {
-            System.out.println(-1);
+            set.add(arr[i]);
         }
-
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= N; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.print(sb);
     }
-
-    public static void calc() {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int card = 1;
-        for (int i = 1; i <= N; i++) {
-            if (degree[i] == 0) pq.add(i);
-        }
-
-        while (!pq.isEmpty()) {
-            int cur = pq.poll();
-            arr[cur] = card++;
-
-            for (int next : in.get(cur)) {
-                if (--degree[next] == 0) pq.add(next);
-            }
-        }
-    }
-
-    public static boolean isCorrect() {
-        for (int i = 1; i < N; i++) {
-            for (int j = i + 1; j <= N; j++) {
-                if (arr[i] > arr[j]) {
-                    if (!out.get(i).contains(j)) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-
 
 }
