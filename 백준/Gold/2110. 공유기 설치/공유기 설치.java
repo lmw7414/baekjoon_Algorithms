ac58a4e017eca33974eca218f5b8096f4104eec0
Minwoo Lee <lmw7414@naver.com>
2025-08-01 01:41:26 +0900

    [Gold IV] Title: 공유기 설치, Time: 260 ms, Memory: 28996 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,51 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Main {
+public class Main {
+    static int N, C;
     static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
         arr = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(br.readLine());
         Arrays.sort(arr);
+        int min = 1;
+        int max = arr[N - 1] - arr[0] + 1;
+        int result = 0;
 
-        int min = 1;  //최소 거리
-        int max = arr[N - 1] - arr[0] + 1;  // 최대 거리 + 1
-        //upper bound
-        while (min < max) {
+        while (min <= max) {
             int mid = (min + max) / 2;
-
-            if (possibleInstallment(mid) < C) {  // 공유기 설치가 남는 경우
-                max = mid;  // 거리를 줄여야 함
-            } else {  // 공유기가 부족한 경우 -> 거리를 늘려야 함
+            if (install(mid) >= C) { // 간격을 늘려 줄여야 한다.
+                result = mid;
                 min = mid + 1;
-            }
+            } else max = mid - 1;
         }
-
-        System.out.println(min - 1);
+        System.out.println(result);
     }
 
-    public static int possibleInstallment(int distance) {
+    public static int install(int dist) {
         int cnt = 1;
-        int recentHouse = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            int curX = arr[i];
-
-            if (curX - recentHouse >= distance) {
+        int place = arr[0];
+        for (int i = 1; i < N; i++) {
+            if (arr[i] - place >= dist) {
                 cnt++;
-                recentHouse = curX;
+                place = arr[i];
             }
         }
         return cnt;
