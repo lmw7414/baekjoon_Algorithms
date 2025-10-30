1bb6acf203a1bd3651dee2e4dfe47256f7f67c1d
Minwoo Lee <lmw7414@naver.com>
2025-10-30 16:27:19 +0900

    [Gold IV] Title: 어드벤처 게임, Time: 348 ms, Memory: 45072 KB -BaekjoonHub

--- 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.java
+++ 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.java
@@ -11,8 +11,8 @@ public class Main {
         while (true) {
             int N = Integer.parseInt(br.readLine());
             if (N == 0) break;
-            Node[] nodes = new Node[N + 1];
             List<Integer>[] adjList = new List[N + 1];
+            int[] costs = new int[N + 1];
             for (int i = 1; i <= N; i++) {
                 adjList[i] = new ArrayList<>();
             }
@@ -20,6 +20,8 @@ public class Main {
                 String[] str = br.readLine().split(" ");
                 char room = str[0].charAt(0);
                 int cost = Integer.parseInt(str[1]);
+                if (room == 'T') cost *= -1;
+                costs[i] = cost;
                 int idx = 2;
                 while (true) {
                     int to = Integer.parseInt(str[idx++]);
@@ -27,65 +29,28 @@ public class Main {
                     if (to == i) continue;
                     adjList[i].add(to);
                 }
-                nodes[i] = new Node(i, room, cost);
             }
-            if (BFS(N, nodes, adjList)) sb.append("Yes\n");
+            boolean[] visit = new boolean[N + 1];
+            visit[1] = true;
+            if (DFS(N, visit, costs, adjList, 1, 0)) sb.append("Yes\n");
             else sb.append("No\n");
         }
         System.out.print(sb);
     }
 
-    public static boolean BFS(int N, Node[] nodes, List<Integer>[] adjList) {
-        int[][] visit = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) Arrays.fill(visit[i], -1);
-        if (nodes[1].room == 'T') return false;
-        PriorityQueue<State> pq = new PriorityQueue<>((a, b) -> b.balance - a.balance);
-        pq.add(new State(1, 0));
-        visit[1][1] = 0;
+    public static boolean DFS(int N, boolean[] visit, int[] costs, List<Integer>[] adjList, int cur, int cost) {
+        if (N == cur) return true;
+        for (int next : adjList[cur]) {
+            if (visit[next]) continue;
 
-        while (!pq.isEmpty()) {
-            State cur = pq.poll();
-
-            for (int next : adjList[cur.roomId]) {
-                char type = nodes[next].room;
-                int cost = nodes[next].cost;
-                int nextCost = 0;
-                if (type == 'E') { // 빈방
-                    nextCost = cur.balance;
-                } else if (type == 'L') { // 레프리콘
-                    nextCost = Math.max(cost, cur.balance);
-                } else { // 트롤
-                    if (cur.balance < cost) continue;
-                    nextCost = cur.balance - cost;
-                }
-                if (nodes[next].id == N) return true;
-                if (visit[cur.roomId][next] >= nextCost) continue; // 이전에 방문한 적 있음
-                visit[cur.roomId][next] = nextCost;
-                pq.add(new State(next, nextCost));
-            }
+            int nextCost = 0;
+            if (costs[next] > 0) nextCost = Math.max(cost, costs[next]);
+            else nextCost = cost + costs[next];
+            if (nextCost < 0) continue;
+            visit[next] = true;
+            if (DFS(N, visit, costs, adjList, next, nextCost)) return true;
+            visit[next] = false;
         }
         return false;
-    }
-
-    static class Node {
-        int id;
-        char room;
-        int cost;
-
-        public Node(int id, char room, int cost) {
-            this.id = id;
-            this.room = room;
-            this.cost = cost;
-        }
-    }
-
-    static class State {
-        int roomId;
-        int balance;
-
-        public State(int roomId, int balance) {
-            this.roomId = roomId;
-            this.balance = balance;
-        }
     }
 }
